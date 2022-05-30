bebfaf8272801c07494d843bf14fd9bfd028e20c
valikhan03 <valikhan.eltaev@gmail.com>
2022-05-30 16:59:32 +0600

    update

--- user_service/handler/handler.go
+++ user_service/handler/handler.go
@@ -1,6 +1,54 @@
 package handler
 
-import(
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+
+	"user_service/models"
 )
 
+type Handler struct {
+	usecase Usecase
+}
+
+type Usecase interface {
+	NewUser(user models.User) error
+	LogIn(email, password string) (string, error)
+}
+
+func (h *Handler) Register(c *gin.Context) {
+	var user models.User
+	err := c.BindJSON(&user)
+	if err != nil {
+
+	}
+
+	err = h.usecase.NewUser(user)
+	if err != nil {
+
+	}
+
+	c.Status(http.StatusCreated)
+}
+
+type loginData struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (h *Handler) LogIn(c *gin.Context) {
+	var loginData loginData
+	err := c.BindJSON(loginData)
+	if err != nil{
+
+	}
+
+	token, err := h.usecase.LogIn(loginData.Email, loginData.Password)
+	if err != nil{
+
+	}
+
+	c.SetCookie("access-token", token, 100000000, "/api/", "", false, true)
+	c.Status(http.StatusOK)
+}
--- user_service/usecase/usecase.go
+++ user_service/usecase/usecase.go
@@ -1,2 +1,43 @@
 package usecase
 
+import(
+	"user_service/models"
+)
+
+type UseCase struct{
+	repository Repository
+	jwt *models.JWTWrapper
+}
+
+type Repository interface{
+	CreateUser(user models.User) error
+	GetUserData(email string) (*models.User, error)
+}
+
+func NewUseCase() {
+
+}
+
+func(uc *UseCase) NewUser(user models.User) error {
+	user.HashPassword()
+	return uc.repository.CreateUser(user)
+}
+
+func(uc *UseCase) LogIn(email, password string) (string, error) {
+	user, err := uc.repository.GetUserData(email)
+	if err != nil{
+		return "", err
+	}
+
+	err = user.CheckPassword(password)
+	if err != nil{
+		return "", err
+	}
+
+	token, err := uc.jwt.GenerateToken(email)
+	if err != nil{
+		return "", err
+	}
+
+	return token, nil
+}
